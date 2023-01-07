cfaba44acd7ef3666107f47cb25c05a370452321
SUMON KHAN <122062318+SUMON-143@users.noreply.github.com>
2023-01-08 01:47:59 +0600

    Update SUMON.py

--- SUMON.py
+++ SUMON.py
@@ -1,7 +1,7 @@
 import os, sys
 os.system('clear')
 print(' Follow My Github Account..... ')
-os.system('xdg-open https://github.com/SPY1x1')
+os.system('xdg-open https://github.com/SUMON-143')
 try:
     __import__("SUMON").Main()
 except Exception as e:
